a560ffad8760f3482a3174cb941fb03c1c8f08c5
Oleksandr Iievliev <sXXXX@pjwstk.edu.pl>
2026-03-10 13:22:16 +0100

    "Some changes to SomeCode.java"

--- SomeCode.java
+++ SomeCode.java
@@ -1,5 +1,17 @@
+import java.util.*;
 public SomeCode {
     public static void main(String[] args) {
+
         System.out.println("Hello world");
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int a = input.nextInt();
+        System.out.print("Enter a second number: ");
+        int b = input.nextInt();
+        System.out.print("Sum: " + (a + b));
+        System.out.println("Diff: " + (a - b));
+        System.out.println("Product: " + (a * b));
+        System.out.println("Result of devision: " + (a / b));
     }
 }
