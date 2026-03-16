088adcf2843c1577e88d03f8d5d9901c25e9387b
Oleksandr Iievliev <s32907@pjwstk.edu.pl>
2026-03-16 21:35:21 +0100

    "Added a body to CalculateAverage method"

--- SomeCode.java
+++ SomeCode.java
@@ -18,7 +18,11 @@ public SomeCode {
     }
     
     public double CalculateAverage(int[] values) {
-    
+    int sum = 0;
+        for (int i = 0; i < values.length ; i++) {
+            sum += values[i];
+        }
+    return (double) sum/values.length;    
     }
     
 }
