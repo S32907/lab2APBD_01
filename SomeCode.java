eb0c75d97c10304184dbd61f0d378e6de21a82a8
Oleksandr Iievliev <s32907@pjwstk.edu.pl>
2026-03-16 22:25:57 +0100

    "modified CalculateAverage method"

--- SomeCode.java
+++ SomeCode.java
@@ -19,11 +19,11 @@ public SomeCode {
     }
     
     public double CalculateAverage(int[] values) {
-    int sum = 0;
+    int sumOfValues = 0;
         for (int i = 0; i < values.length ; i++) {
-            sum += values[i];
+            sumOfValues += values[i];
         }
-    return (double) sum/values.length;    
+    return (double) sumOfValues/values.length;    
     }
     
     public int CalculateMax(int[] values) {
